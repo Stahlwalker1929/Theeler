eadf2bf689a2b2ca7a516c376709a6d37fa6a0eb
Dustin Lee Stahl </o=Exchange Organization/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=dstahl>
2018-02-06 14:57:14 -0600

    Our first comment at Git

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func buttonChange(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+        if buttonCount >= 10 {
+        view.backgroundColor = UIColor.red
+        
+            myLabel.text = "2018"}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myLabel.text = "Sunset Safari"
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
