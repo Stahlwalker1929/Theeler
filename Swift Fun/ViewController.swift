23e4591bd242e95fc778239c8540c6388af51bce
Unknown <dustin.stahl@stahlwalker.com>
2018-02-06 15:09:38 -0600

    Deleted an extra line.
    Done in GitKraken

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -27,7 +27,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         myLabel.text = "Sunset Safari"
 
-
         
     }
 
